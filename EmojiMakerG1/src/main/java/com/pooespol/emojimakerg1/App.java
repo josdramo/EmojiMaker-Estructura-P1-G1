f6879604e3c359ecc6b930f43195b7995ece33d8
Christopher Díaz <nealthmc@gmail.com>
2023-07-10 18:32:11 -0500

    connect auth with app class

--- EmojiMakerG1/src/main/java/com/pooespol/emojimakerg1/App.java
+++ EmojiMakerG1/src/main/java/com/pooespol/emojimakerg1/App.java
@@ -12,27 +12,23 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
-
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
     public static void main(String[] args) {
         launch();
     }
 
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/auth.fxml"));
+            Scene scene = new Scene(root);
+
+            stage.setTitle("Project Yoshino");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+   
 }
---
+++ EmojiMakerG1/src/main/java/controllers/AuthController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author infrative
+ */
+public class AuthController {
+    @FXML
+    private VBox container;
+    
+    @FXML
+    private TextField usernameField;
+    
+    @FXML
+    private TextField passwordField;
+    
+    public void initialize() {
+        System.out.println("Hosss");
+    }
+    
+    public void onLogin() {
+
+    }
+    
+    public void onRegister() {
+        
+    }
+}
