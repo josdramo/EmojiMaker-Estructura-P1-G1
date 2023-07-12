7135e572339fdc4036f553ee1d8796a636c03b1e
Christopher Díaz <nealthmc@gmail.com>
2023-07-11 22:38:29 -0500

    add emoji to consult emojis

--- EmojiMakerG1/src/main/java/Controllers/ConsultarEmojisController.java
+++ EmojiMakerG1/src/main/java/Controllers/ConsultarEmojisController.java
@@ -4,7 +4,11 @@
  */
 package Controllers;
 
+import Modelos.Emoji;
 import com.pooespol.emojimakerg1.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -13,8 +17,26 @@ import com.pooespol.emojimakerg1.App;
 public class ConsultarEmojisController {
     private App app;
     
+    @FXML
+    private VBox container;
+    
+    @FXML
+    private ListView<Emoji> emojisListView;
+    
     public void onVolverAlMenu() {
         app.switchToMenuPrincipal();
+    }
+    
+    public void onAgregar() {
+        app.openCreateEmoticonModal();
+    }
+    
+    public void onEditar() {
+        // TODO
+    }
+    
+    public void onEliminar() {
+        // TODO
     }
 
     public void setApp(App app) {
