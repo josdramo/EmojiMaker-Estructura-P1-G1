9f2253f2a35664b0ada648038553357c65a9a8b8
“josdramo” <josdramo@espol.edu.ec>
2023-07-12 16:21:02 -0500

    Sistema de archivos para agregar imagenes

--- EmojiMakerG1/src/main/java/Controllers/MenuPrincipalController.java
+++ EmojiMakerG1/src/main/java/Controllers/MenuPrincipalController.java
@@ -5,27 +5,55 @@
 package Controllers;
 
 import com.pooespol.emojimakerg1.App;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
  *
  * @author infrative
  */
 public class MenuPrincipalController {
+
     private App app;
-    
+
     @FXML
     private VBox container;
-    
-    public void onConsultarComponentes() {
-        // TODO
+
+    @FXML
+    void onConsultarComponentes(ActionEvent event) {
+        String rutaBaseProyecto = System.getProperty("user.dir");
+        String rutaCarpetaDestino = rutaBaseProyecto + "/src/main/resources/images";
+
+        File carpetaDestino = new File(rutaCarpetaDestino);
+        FileChooser archivoEscogido = new FileChooser();
+
+        ExtensionFilter filtroPNG = new ExtensionFilter("Archivos PNG", "*.png");
+        archivoEscogido.getExtensionFilters().add(filtroPNG);
+
+        File archivoImagen = archivoEscogido.showOpenDialog(null);
+        if (archivoImagen != null) {
+            try{
+             Path rutaDestino = carpetaDestino.toPath().resolve(archivoImagen.getName());
+
+ 
+            Files.copy(archivoImagen.toPath(), rutaDestino, StandardCopyOption.REPLACE_EXISTING);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
-    
+
     public void onConsultarEmojis() {
         app.switchToConsultarEmojis();
     }
-    
+
     public void onSalir() {
         app.destroySession();
     }
@@ -33,5 +61,5 @@ public class MenuPrincipalController {
     public void setApp(App app) {
         this.app = app;
     }
-    
+
 }
